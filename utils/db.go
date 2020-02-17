79bfd0e25fa67ada9b95ab21826b2c6ef7dafc4a
651890297 <651890fuzejun>
2020-02-17 13:55:53 +0800

    bookstore 完成用户、图书分页

---
+++ utils/db.go
@@ -0,0 +1,18 @@
+package utils
+
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var (
+	Db  *sql.DB
+	err error
+)
+
+func init() {
+	Db, err = sql.Open(`mysql`, `root:123456@tcp(127.0.0.1:3306)/bookstore0612?charset=utf8&parseTime=true`)
+	if err != nil {
+		panic(err.Error())
+	}
+}
